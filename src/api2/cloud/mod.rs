acb01bd0df8625cd4ac260b35a4e74da28d48122
shoaib-khalid <shoaib@kalsym.com>
2024-01-24 16:44:16 +0500

    adding cloud apis

---
+++ src/api2/cloud/mod.rs
@@ -0,0 +1,34 @@
+//! Cloud Backup Management
+
+use anyhow::Error;
+use serde_json::Value;
+
+use proxmox_router::{list_subdirs_api_method, Router, SubdirMap};
+use proxmox_schema::api;
+
+
+#[api(
+    input: {
+        properties: {},
+    },
+    returns: {
+        description: "Cloud hello world.",
+        type: String,
+    },
+)]
+/// Cloud Hello
+pub fn cloud_hello(_param: Value) -> Result<String, Error> {
+    Ok("api2/json/cloud/cloud-hello cloud-hello-world".to_string())
+}
+
+const SUBDIRS: SubdirMap = &[
+    (
+        "cloud-hello",
+        &Router::new().get(&API_METHOD_CLOUD_HELLO),
+    ),
+    ("scan-drives", &Router::new().get(&API_METHOD_SCAN_DRIVES)),
+];
+
+pub const ROUTER: Router = Router::new()
+    .get(&list_subdirs_api_method!(SUBDIRS))
+    .subdirs(SUBDIRS);
